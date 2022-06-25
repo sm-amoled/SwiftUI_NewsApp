7b83b313605db30636831253e31307d5ac22e7e0
sm-amoled <moktak072@naver.com>
2022-06-25 23:26:32 +0900

    WatchOS 45:00

---
+++ SwiftUI_NewsApp/SwiftUI-NewsApp-Watch WatchKit Extension/Views/ArticleDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleDetailView.swift
+//  SwiftUI-NewsApp-Watch WatchKit Extension
+//
+//  Created by Park Sungmin on 2022/06/25.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleDetailView()
+    }
+}
